d92f16cee67d0bac595df51ddc651f796350613b
David Muchu Suh <dsmuchu@gmail.com>
2020-01-03 00:36:51 +0100

    update forecast string to include high/low temp

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location.', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + 'The temperature today ranges from a high of ' + body.daily.data[0].temperatureHigh + ' to a low of ' + body.daily.data[0].temperatureLow + ' . It is currently ' + body.currently.temperature + ' degrees out. There is '+ body.currently.precipProbability  * 100  + ' % chance of rain.')
+            callback(undefined, body.daily.data[0].summary + 'The high today is ' + body.daily.data[0].temperatureHigh + ' and the low is ' + body.daily.data[0].temperatureLow + ' . It is currently ' + body.currently.temperature + ' degrees out. There is '+ body.currently.precipProbability  * 100  + ' % chance of rain.')
         }
     })
 }
